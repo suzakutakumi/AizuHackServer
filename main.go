06187adb99d4ecc1d41dfbe0eb5928dc45b2d80e
suzakutakumi <suzakutakumi3@gmail.com>
2022-06-16 11:46:06 +0900

    テスト作成

---
+++ main.go
@@ -0,0 +1,11 @@
+package AizuHackServer
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HelloGet is an HTTP Cloud Function.
+func HelloGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
